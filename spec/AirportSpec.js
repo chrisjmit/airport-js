8ab00c2e3a892fe885c5350d8833c68ea3fb692c
anaalta <suciuanamaria@yahoo.com>
2017-05-09 18:08:23 +0200

    added a hangar, cleared for landing functionalities

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,9 +1,21 @@
 /*jshint strict:false*/
 
-describe('Airport',function(){
+describe('Feature test:', function(){
+  var plane;
   var airport;
+
+  beforeEach(function(){
   airport = new Airport();
-  it ("has no planes by default", function(){
-    expect(airport.planes()).toEqual([]);
+  plane = new Plane();
   });
+    it('planes can be instructed to land', function(){
+      plane.land(airport);
+      expect(airport.planes()).toContain(plane);
+    });
+
+    it ('can clear planes for landing', function(){
+      airport.clearForLanding(plane);
+      expect(airport.planes()).toEqual([plane]);
+    });
+
 });
