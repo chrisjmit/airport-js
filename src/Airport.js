8d1ce1cbb540e39bcd9191ae8f2c481a8a8199cf
anaalta <suciuanamaria@yahoo.com>
2017-05-09 18:11:37 +0200

    with hangar in

--- src/Airport.js
+++ src/Airport.js
@@ -1,9 +1,13 @@
 /*jshint strict:false*/
 
 function Airport() {
+  this._hangar = [];
 
   Airport.prototype.planes = function(){
-    
+    return this._hangar;
   };
 
+  Airport.prototype.clearForLanding = function(plane){
+    this._hangar.push(plane);
+  };
 }
--- src/Plane.js
+++ src/Plane.js
@@ -2,6 +2,8 @@
 
 function Plane() {
 
-Plane.prototype.land = function(){};
+Plane.prototype.land = function(airport){
+  airport.clearForLanding(this);
+};
 
 }
